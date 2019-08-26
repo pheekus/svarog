601414918369a694e1e2ee7fd52f15fd9009bdc1
dantothefuture <dantothefuture@gmail.com>
2019-08-26 12:02:41 +0300

    feat(compiler): throw when no $id found in schema

--- src/compile/index.ts
+++ src/compile/index.ts
@@ -3,6 +3,10 @@ import cel from './cel';
 import compileType from './compile-type';
 
 export default function(schema: JSONSchema7): string {
+  if (typeof schema.$id !== 'string') {
+    throw new Error('Schema $id is required to name the validator function');
+  }
+
   const dataRef = cel.ref('d');
   const strictRef = cel.ref('s');
   const validator = compileType(schema, dataRef, strictRef);
